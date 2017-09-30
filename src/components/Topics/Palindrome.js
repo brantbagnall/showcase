a82281a16ec1ca971937be8da6f99c8c8a788e7d
Brant Bagnall <brant.bagnall@gmail.com>
2017-09-29 21:59:57 -0600

    Finished fourth problem

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,13 +1,49 @@
 import React, {Component} from 'react';
 
 class Palindrome extends Component {
-//   constructor() {
-// super();
-//   }
+  constructor() {
+    super();
+
+    this.state = {
+      userInput: '',
+      palindrome: ''
+    }
+  }
+
+  updateInput(e){
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+
+  updateAnswer(){
+    var user = this.state.userInput;
+    var answer ='NO TEXT ENTERED';
+    var reverse = user.split('').reverse().join('');
+    if (user.toLowerCase() ==  reverse.toLowerCase() && user){
+      answer = 'True'
+    } else if (user) {
+      answer = 'False'
+    }
+    this.setState({
+      palindrome: answer
+    });
+  }
 
   render() {
     return (
-<p>Palindrome</p>
+      <div className="puzzleBox filterStringPB">
+        <h4>
+          Palindrome
+        </h4>
+        <input className="inputLine" onChange={ e => this.updateInput(e)} />
+        <button className="confirmationButton" onClick={ ()=> this.updateAnswer()}>
+          Confirm
+        </button>
+        <span className="resultsBox">
+          Palindrome: {this.state.palindrome}
+        </span>
+      </div>
     )
   }
 }
