cccbbb6fe4c831e97831a27f180064f41dde2ef0
Brant Bagnall <brant.bagnall@gmail.com>
2017-09-29 21:32:52 -0600

    finished three problems

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,13 +1,58 @@
 import React, {Component} from 'react';
 
 class FilterString extends Component {
-//   constructor() {
-// super();
-//   }
+  constructor() {
+    super();
+    this.state = {
+      unfilteredArray: ['James', 'Jessica', 'Melody', 'Tyler', 'Blake', 'Jennifer', 'Mark'],
+      userInput: '',
+      filteredArray: []
+    }
+  }
+
+  updateInput(e){
+    this.setState({
+      userInput: e.target.value
+    });
+    console.log(e.target.value);
+  }
+
+  updatArray(){
+    var unArr = this.state.unfilteredArray.slice();
+    var user = this.state.userInput;
+    var reg = new RegExp (user, "i");
+    var fArr = [];
+    unArr.forEach(function(word) {
+      if(word.match(reg) && user.length > 0){
+        console.log(word);
+        fArr.push(word);
+      } else if (user === '') {
+        fArr = ['No Match']
+      }
+    });
+
+    this.setState({
+      filteredArray: fArr
+    });
+  }
 
   render() {
     return (
-<p>FileString</p>
+      <div className="puzzleBox filterStringPB">
+        <h4>
+          Filter String
+        </h4>
+        <span className="puzzleText">
+        Unfiltered: {JSON.stringify(this.state.unfilteredArray)}
+        </span>
+        <input className="inputLine" onChange={ e=> this.updateInput(e)} />
+        <button className="confirmationButton" onClick={ ()=> this.updatArray()}>
+          Confirm
+        </button>
+        <span className="resultsBox filterStringRB">
+        Filtered: {JSON.stringify(this.state.filteredArray)}
+        </span>
+      </div>
     )
   }
 }
